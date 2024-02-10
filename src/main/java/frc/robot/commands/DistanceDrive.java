7966fd936ef42206a8afc6cb7ee1f8b97fa48d97
Carter Malbraaten <cartermalb@outlook.com>
2024-02-09 18:41:45 -0600

    added more unfinished code

--- src/main/java/frc/robot/commands/DistanceDrive.java
+++ src/main/java/frc/robot/commands/DistanceDrive.java
@@ -1,7 +1,29 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.drive.Drive;
+
 public class DistanceDrive {
-  // public static Command //DistanceDrive(){
-  // Drive()
+
+  private DistanceDrive() {}
+
+  public static Command DistanceDrive(Drive drive) {
+    return Commands.run(
+        () -> {
+          double speed = 0.5;
+          double driveDistance = speed * 5; // five seconds of driving.
+          // final Pose2d startingPose;
+          // if (isFirstTime) {
+          // startingPose = drive.getPose();
+          // }
+          double distanceTraveled = drive.getPose().getX(); // /!< TODO: This is probably wrong
+          if (distanceTraveled > driveDistance) {
+            speed = 0;
+          }
+          drive.runVelocity(
+              ChassisSpeeds.fromFieldRelativeSpeeds(speed, 0, 0, drive.getRotation()));
+        });
+  }
 }
-// }
