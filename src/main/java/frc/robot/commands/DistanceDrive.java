d06ed9a97554cb02c4a19ffcde73ab7c42c50d54
Carter Malbraaten <cartermalb@outlook.com>
2024-02-13 20:03:33 -0600

    fixed version issue

--- src/main/java/frc/robot/commands/DistanceDrive.java
+++ src/main/java/frc/robot/commands/DistanceDrive.java
@@ -1,25 +1,27 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.drive.Drive;
+// import edu.wpi.first.math.geometry.Pose2d;
 
 public class DistanceDrive {
 
-  private DistanceDrive() {}
+  // private autoDrive() {}
 
-  public static Command distanceDrive(Drive drive, Encoder encoder) {
-    return Commands.run(
-        () -> {
-          double speed = 0.5;
-          double driveDistance = speed * 5; // five seconds of driving
-          double distanceTraveled = Drive.encoder.getDistance();
+  // public static Command distanceDrive(Drive drive, Encoder encoder) {
+  // return Commands.run(
+  // () -> {
 
-          while (distanceTraveled < driveDistance) {
-            drive.runVelocity(
-                ChassisSpeeds.fromFieldRelativeSpeeds(speed, 0, 0, drive.getRotation()));
-          }
-        });
-  }
+  double speed = 0.5;
+  double driveDistance = speed * 5; // five seconds of driving
+  // double distanceTraveled = Drive.encoder.getDistance();
+
+  // while (distanceTraveled < driveDistance) {
+  // drive.runVelocity(
+  // ChassisSpeeds.fromFieldRelativeSpeeds(speed,
+  // 0,
+  // 0,
+  // drive.getRotation()));
 }
+
+        // }//);
+  // }
+// }
