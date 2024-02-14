6a5cdad965575051dabb19f3857f6bfea0f53c14
Carter Malbraaten <cartermalb@outlook.com>
2024-02-13 19:56:42 -0600

    weird file mismatch commit

--- src/main/java/frc/robot/commands/DistanceDrive.java
+++ src/main/java/frc/robot/commands/DistanceDrive.java
@@ -1,27 +1,25 @@
 package frc.robot.commands;
 
-// import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.drive.Drive;
 
 public class DistanceDrive {
 
-  // private autoDrive() {}
+  private DistanceDrive() {}
 
-  // public static Command distanceDrive(Drive drive, Encoder encoder) {
-  // return Commands.run(
-  // () -> {
+  public static Command distanceDrive(Drive drive, Encoder encoder) {
+    return Commands.run(
+        () -> {
+          double speed = 0.5;
+          double driveDistance = speed * 5; // five seconds of driving
+          double distanceTraveled = Drive.encoder.getDistance();
 
-  double speed = 0.5;
-  double driveDistance = speed * 5; // five seconds of driving
-  // double distanceTraveled = Drive.encoder.getDistance();
-
-  // while (distanceTraveled < driveDistance) {
-  // drive.runVelocity(
-  // ChassisSpeeds.fromFieldRelativeSpeeds(speed,
-  // 0,
-  // 0,
-  // drive.getRotation()));
+          while (distanceTraveled < driveDistance) {
+            drive.runVelocity(
+                ChassisSpeeds.fromFieldRelativeSpeeds(speed, 0, 0, drive.getRotation()));
+          }
+        });
+  }
 }
-
-        // }//);
-  // }
-// }
