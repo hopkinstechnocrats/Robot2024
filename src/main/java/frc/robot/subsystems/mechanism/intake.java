9bd878453d461464205025f0a0a3c3e660b4c6f9
Carter Malbraaten <cartermalb@outlook.com>
2024-02-22 18:27:35 -0600

    added intake file

---
+++ src/main/java/frc/robot/subsystems/mechanism/intake.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.mechanism;
+
+//import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class intake {
+    //CANSparkMax intake_motor = new CANSparkMax(3, CANSparkMax.kbrushless);
+}
