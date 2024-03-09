92636028ce6abf5e06b0a4efe70928b31c895187
Carter Malbraaten <cartermalb@outlook.com>
2024-03-09 12:58:49 -0600

    added enfdeffector and intake files
    didnt have endeffector and intake files.  Those have been added.  Still running into gradle issue
    
    Co-Authored-By: tatewelty <87451665+tatewelty@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/swervedrive/Intake.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.swervedrive;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+public class Intake extends SubsystemBase {
+  CANSparkMax intakeMotor;
+
+  public Intake() {
+    intakeMotor = new CANSparkMax(IntakeConstants.kIntakeMotorPort, MotorType.kBrushless);
+  }
+
+  public void spinIntake() {
+    intakeMotor.set(IntakeConstants.intakeSpeed);
+  }
+
+  public void noSpinIntake() {
+    intakeMotor.set(0);
+  }
+}
