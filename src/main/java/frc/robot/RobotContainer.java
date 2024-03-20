e66aa24279b6c66e262f6f98e4be68091b07e49d
Carter Malbraaten <cartermalb@outlook.com>
2024-03-19 19:45:04 -0500

    path_name_test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -157,7 +157,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("TatePath");
     //return autoChooser.getSelected();
   
 
