031c6d88bed656fcd82850b06056bedb56e3175e
laptop7 <136532458+Freezerat@users.noreply.github.com>
2024-03-20 20:15:00 -0500

    tested a few things, stuff didnt work :(

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -160,7 +160,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("SamplePath.path");
     //return autoChooser.getSelected();
   
 
